be36707ad946a1382ed5030a3f880365906eb21b
mashruzzaman <mashruzzamansaad@gmail.com>
2025-10-19 19:50:29 +0600

    Fix port binding for Render

--- app.py
+++ app.py
@@ -866,5 +866,9 @@ def predict():
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
